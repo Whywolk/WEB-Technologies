27c107a76c5d8a1d476001a90fdbaf5745c6f3a7
Whywolk <alexshir2001@gmail.com>
2021-09-17 18:39:07 +0300

    Added style for inputs

--- lab1/logic.js
+++ lab1/logic.js
@@ -1,22 +1,24 @@
 var month = 0;
-var curr = "f";
 
 function calcSalary()
 {
-    var salForm = document.getElementsByName("sal");
+    var salForm = document.getElementsByClassName("inputMoney");
     var monthCounts = 3;
 
     var count;
     var averageSalary = 0;
+
     for(count = 0; count < salForm.length && count < monthCounts; count++)
     {
         averageSalary += parseInt(salForm[count].value);
     }
     averageSalary /= count;
+
     var salaryField = document.getElementById("salaryVal");
-    salaryField.innerHTML = averageSalary.toFixed(3);
+    salaryField.innerHTML = averageSalary.toFixed(2);
+
     var description = document.getElementById("description");
-    description.innerHTML = " "+ curr + " for last " + count + " month";
+    description.innerHTML = " for last " + count + " month";
 }
 
 function addForm()
@@ -24,16 +26,20 @@ function addForm()
     month++;
     var inputs = document.getElementById("firstInputs");
     var div = document.createElement("div");
-    div.innerHTML = '<br>' + '<span>Month ' + month + ': </span>';
-    var form = '<input name="sal" type="number" value="0" size="10" onblur="calcSalary()">'; 
+    div.className = "inputDiv";
+    div.innerHTML = '<span>Month ' + month + ': </span>';
+
+    var form = '<input class="inputMoney" type="number" value="0" onblur="calcSalary()">'; 
     div.innerHTML += form;
 
     inputs.after(div);
 }
 
-function getCurrency()
+function setCurrency()
 {
-    var list = document.getElementById("currency");
-    curr = list.options[list.selectedIndex].text;
-    calcSalary();
+    var selectList = document.getElementById("currencySelect");
+    var curr = selectList.options[selectList.selectedIndex].text;
+
+    var currField = document.getElementById("salaryCurrency");
+    currField.innerHTML = curr;
 }
