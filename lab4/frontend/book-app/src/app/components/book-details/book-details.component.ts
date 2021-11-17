d62248ff1eee46a080a7d52e30d9c01b78517d72
Whywolk <alexshir2001@gmail.com>
2021-11-17 19:15:39 +0300

    Added BookDetails component
    For view full book description, also for change and delete one

---
+++ lab4/frontend/book-app/src/app/components/book-details/book-details.component.ts
@@ -0,0 +1,75 @@
+import {Component, Input, OnInit} from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+import { BookService } from "../../services/book.service";
+import { Book } from "../../models/book.model";
+
+@Component({
+  selector: 'app-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.css']
+})
+export class BookDetailsComponent implements OnInit {
+
+  book: Book = {};
+  isChanging = false;
+  message = '';
+
+  constructor(
+    private bookService: BookService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+    this.getBook(this.route.snapshot.params["id"]);
+  }
+
+  change(): void {
+    this.isChanging = true;
+  }
+
+  getBook(id: number | undefined): void {
+    this.bookService.get(id)
+      .subscribe({
+        next: (data) => {
+          console.log(data);
+          this.book = data;
+        }
+      })
+  }
+
+  cancel(): void{
+    this.isChanging = false;
+  }
+
+  updateBook(): void {
+    if (!this.book.title) {
+      this.message = 'Добавьте название!';
+      return;
+    }
+
+    this.message = '';
+    this.bookService.update(this.book.id, this.book)
+      .subscribe({
+        next: (data) =>{
+          console.log(data);
+          this.book = data;
+          this.isChanging = false;
+        },
+        error: (e) => console.error(e)
+      });
+  }
+
+  deleteBook(): void {
+    this.bookService.delete(this.book.id)
+      .subscribe({
+        next: (res) => {
+          console.log(res);
+          this.router.navigate(['/books'])
+        },
+        error: (e) => {
+          console.log(e);
+        }
+      });
+  }
+}
