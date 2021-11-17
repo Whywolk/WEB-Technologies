3a23a09e91d08949fcebc1cd9dc2b3b1e85abe9a
Whywolk <alexshir2001@gmail.com>
2021-11-17 19:12:16 +0300

    Book service for REST requests

---
+++ lab4/frontend/book-app/src/app/services/book.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Book } from "../models/book.model";
+
+const apiUrl = 'http://localhost:8080/api'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  constructor(private http: HttpClient) { }
+
+  getAll(): Observable<Book[]> {
+    return this.http.get<Book[]>(`${apiUrl}/books`);
+  }
+
+  get(id: any): Observable<Book> {
+    return this.http.get<Book>(`${apiUrl}/book/${id}`);
+  }
+
+  create(data: any): Observable<any> {
+    return this.http.post(`${apiUrl}/books`, data);
+  }
+
+  update(id: any, data: any): Observable<any> {
+    return this.http.put(`${apiUrl}/book/${id}`, data);
+  }
+
+  delete(id: any): Observable<any> {
+    return this.http.delete(`${apiUrl}/book/${id}`);
+  }
+}
