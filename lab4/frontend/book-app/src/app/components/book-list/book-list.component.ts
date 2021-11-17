5f305f79d9a751cae819b2b4e0564a36d91c456a
Whywolk <alexshir2001@gmail.com>
2021-11-17 19:14:14 +0300

    Added BookList component
     For accessing all books

---
+++ lab4/frontend/book-app/src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from "../../models/book.model";
+import { BookService } from "../../services/book.service";
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  books?: Book[];
+  currentBook: Book = {};
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.retrieveBooks();
+  }
+
+  retrieveBooks(): void {
+    this.bookService.getAll()
+      .subscribe({
+        next: (data) => {
+          this.books = data;
+          console.log(data);
+        },
+        error: (e) => console.error(e)
+      })
+  }
+
+}
